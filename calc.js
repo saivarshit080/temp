d4e7515ecc7c53b2e9b9b4fd49755d00439102a2
saivarshit080 <saivarshit08@gmail.com>
2025-03-06 10:44:43 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+export {add,subtract,multipy,division}
+
+
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multipy(a,b){
+    return a*b
+}
+function division(a,b){
+    return a/b
+}
+
---
+++ main.js
@@ -0,0 +1,10 @@
+import {add,subtract,multipy,division} from "./calc.js"
+
+let result = add(2,3)
+console.log(result)
+let result1 = subtract(9,2)
+console.log(result1)
+let result2 = multipy(9,2)
+console.log(result2)
+let result3 = division(9,2)
+console.log(result3)
